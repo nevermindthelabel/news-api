205750eb621c0b39d445d6e3c2630c8e12c71d44
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-02 15:49:46 -0700

    initial navigation component

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+const Navigation = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">News API</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/about/">About</NavLink>
+            </NavItem>
+            </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navigation;
