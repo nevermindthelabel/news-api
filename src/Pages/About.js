780526f8e24d266658627f9a387cc033d616ffbe
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-02 21:43:09 -0700

    added basic about page

---
+++ src/Pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+const About = () => {
+  return (
+    <Container className='main'>
+      <Row>
+        <Col>
+          <p>This is the about page</p>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default About;
