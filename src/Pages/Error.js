67635b5a8b8b46105b02b38b6cb21fdb44d52d97
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-02 21:41:55 -0700

    added basic error page

---
+++ src/Pages/Error.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Container, Row, Col} from 'reactstrap';
+
+const Error = () => {
+  return(
+    <Container className='main'>
+      <Row>
+        <Col>
+          <h1>
+            Error
+          </h1>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Error;
