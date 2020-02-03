d1807485c6bf405590c412b63c72dc58e87a2ff4
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-02 22:48:42 -0700

    added styles and reworked component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,13 +5,14 @@ import {
 } from 'reactstrap';
 import axios from 'axios';
 import API from '../../util';
+import './style.css';
 
 const Cards = () => {
   const [articles, setArticles] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${API}`).then(data => {
-      setArticles(data.data.articles);setIsLoaded(true)
+      setArticles(data.data.articles); setIsLoaded(true)
     })
   })
   return (
@@ -19,12 +20,10 @@ const Cards = () => {
       {!isLoaded ? <h1>spinning</h1> : articles.map(article => <Card>
         <CardImg src={article.urlToImage} alt={article.title} />
         <CardBody>
-      <CardTitle>{article.title}</CardTitle>
-      <CardSubtitle>{article.author}</CardSubtitle>
-          <CardText>
-            {article.content}
-          </CardText>
-          <Button>button</Button>
+          <CardTitle><h1>{article.title}</h1></CardTitle>
+          <CardSubtitle><h3>By: {article.author}</h3></CardSubtitle>
+          <CardText dangerouslySetInnerHTML={{__html: article.content}} />
+          <Button>Read More</Button>
         </CardBody>
       </Card>)}
     </Fragment>
