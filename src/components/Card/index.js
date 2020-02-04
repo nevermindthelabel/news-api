9be6958654b4b5cb6ac4b9a1ccfb6d5965454890
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-03 20:20:17 -0700

    added link to article

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -11,7 +11,7 @@ const Cards = () => {
   const [articles, setArticles] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
-    axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${API}`).then(data => {
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API}`).then(data => {
       setArticles(data.data.articles); setIsLoaded(true)
     })
   })
@@ -20,7 +20,7 @@ const Cards = () => {
       {!isLoaded ? <h1>spinning</h1> : articles.map(article => <Card>
         <CardImg src={article.urlToImage} alt={article.title} />
         <CardBody>
-          <CardTitle><h1>{article.title}</h1></CardTitle>
+          <CardTitle><a href={article.url}><h1>{article.title}</h1></a></CardTitle>
           <CardSubtitle><h3>By: {article.author}</h3></CardSubtitle>
           <CardText dangerouslySetInnerHTML={{__html: article.content}} />
           <Button>Read More</Button>
