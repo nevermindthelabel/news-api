591582bd105ebee83b552110e9624fa07c390bf7
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-05 22:56:02 -0700

    updated text on About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,7 +6,13 @@ const About = () => {
     <Container className='main'>
       <Row>
         <Col>
-          <p>This is the about page</p>
+          <h1 className='mt-3 mb-3'>About</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>This project was created with Create React App and the News API. Currently, it pulls in the top news stories from the US and displays them on the home page, taking advantage of the News API's robust API. </p>
+          <p>Future planned updates include creating a seperate search page where someone can search for news on a particular topic, or news by country. The News API allows you to parse the data by topic, country, or search timeframe.</p>
         </Col>
       </Row>
     </Container>
