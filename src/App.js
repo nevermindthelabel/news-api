cfd269da885bd185daf79fd00195236caa88f4c8
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-05 21:33:43 -0700

    updated home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 import About from './Pages/About';
 import Home from './Pages/Home';
+import Search from './Pages/Search';
 import Error from './Pages/Error';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
+        <Route exact path='/search' component={Search} />
         <Route component={Error} />
       </Switch>
       <Footer />
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,25 +1,18 @@
-import React, { useState, UseEffect } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import API from '../util';
 import Cards from '../components/Card';
 
 const Home = () => {
-  const [articles, setArticles] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  // useEffect(() => {
-  //   fetch(`https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=${API}`).then(response => response.json()).then(data => setArticles(data.articles).then(setIsLoaded(true)))
-  // })
+
   return (
     <Container className='main'>
       <Row>
         <Col>
-          <h1>Welcome Home</h1>
+          <h1>Top News</h1>
         </Col>
       </Row>
       <Row>
-        <Col>
-          <Cards />
-        </Col>
+        <Cards />
       </Row>
     </Container>
 
