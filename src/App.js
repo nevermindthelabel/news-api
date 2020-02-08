9b67a79880a6fa158416ed7134c637937a1516ca
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-07 22:05:47 -0700

    added search page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 import About from './Pages/About';
 import Home from './Pages/Home';
-// import Search from './Pages/Search';
+import Search from './Pages/Search';
 import Error from './Pages/Error';
 
 function App() {
@@ -14,7 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
-        {/* <Route exact path='/search' component={Search} /> */}
+        <Route exact path='/search' component={Search} />
         <Route component={Error} />
       </Switch>
       <Footer />
