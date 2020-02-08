6ffb5cf4be511307c6f024f452f338d5ed902a73
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-07 22:03:22 -0700

    moved logic to page level component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Cards from '../components/Card';
+import axios from 'axios';
+import API from '../util';
 
 const Home = () => {
+
+  const [articles, setArticles] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API}`).then(data => {
+      setArticles(data.data.articles); setIsLoaded(true)
+    })
+  })
 
   return (
     <Container className='main'>
@@ -11,8 +21,10 @@ const Home = () => {
           <h1>Top News</h1>
         </Col>
       </Row>
-      <Row>
-        <Cards />
+      <Row>{!isLoaded ? <h1>loading</h1> : articles.map((article, index) => (
+        <Cards key={index} article={article}/>
+      ))
+      }
       </Row>
     </Container>
 
