eb041b92a7d71ad696d5063316ca04f250564ebb
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-07 22:04:53 -0700

    moved logic to page for reusability

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,32 +1,23 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React from 'react';
 import {
   Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Spinner
 } from 'reactstrap';
-import axios from 'axios';
-import API from '../../util';
 import './style.css';
 
-const Cards = () => {
-  const [articles, setArticles] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API}`).then(data => {
-      setArticles(data.data.articles); setIsLoaded(true)
-    })
-  })
+const Cards = (props) => {
+
   return (
-    <Fragment>
-      {!isLoaded ? <Spinner style={{ width: '3rem', height: '3rem', margin: 'auto' }} /> : articles.map(article => <Card>
-        <CardImg src={article.urlToImage} alt={article.title} />
-        <CardBody>
-          <CardTitle><a href={article.url} target='_blank' rel='noopener noreferrer'><h1>{article.title}</h1></a></CardTitle>
-          <CardSubtitle><h3>By: {article.author}</h3></CardSubtitle>
-          <CardText dangerouslySetInnerHTML={{__html: article.content}} />
-          <a href={article.url} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Read More</a>
-        </CardBody>
-      </Card>)}
-    </Fragment>
+
+    <Card>
+      <CardImg src={props.article.urlToImage} alt={props.article.title} />
+      <CardBody>
+        <CardTitle><a href={props.article.url} target='_blank' rel='noopener noreferrer'><h1>{props.article.title}</h1></a></CardTitle>
+        <CardSubtitle><h3>By: {props.article.author}</h3></CardSubtitle>
+        <CardText dangerouslySetInnerHTML={{ __html: props.article.content }} />
+        <a href={props.article.url} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Read More</a>
+      </CardBody>
+    </Card>
   )
 }
 
