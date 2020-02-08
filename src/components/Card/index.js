54a9b08092b45187259065630a11aeb218fefa04
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-07 22:07:35 -0700

    destructured props

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,17 +5,17 @@ import {
 } from 'reactstrap';
 import './style.css';
 
-const Cards = (props) => {
+const Cards = ({ article }) => {
 
   return (
 
     <Card>
-      <CardImg src={props.article.urlToImage} alt={props.article.title} />
+      <CardImg src={article.urlToImage} alt={article.title} />
       <CardBody>
-        <CardTitle><a href={props.article.url} target='_blank' rel='noopener noreferrer'><h1>{props.article.title}</h1></a></CardTitle>
-        <CardSubtitle><h3>By: {props.article.author}</h3></CardSubtitle>
-        <CardText dangerouslySetInnerHTML={{ __html: props.article.content }} />
-        <a href={props.article.url} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Read More</a>
+        <CardTitle><a href={article.url} target='_blank' rel='noopener noreferrer'><h1>{article.title}</h1></a></CardTitle>
+        <CardSubtitle><h3>By: {article.author}</h3></CardSubtitle>
+        <CardText dangerouslySetInnerHTML={{ __html: article.content }} />
+        <a href={article.url} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Read More</a>
       </CardBody>
     </Card>
   )
