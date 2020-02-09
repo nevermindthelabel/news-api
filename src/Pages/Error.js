e26d91a77c85821b274b3dc28e30e9821b331c22
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-09 16:01:46 -0700

    added banner image

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
+import Image from '../images/banner.png';
+import HeroImage from '../components/HeroImage'
 
 const Error = () => {
-  return(
-    <Container className='main'>
-      <Row>
-        <Col>
-          <h1>
-            Error
+  return (
+    <>
+      <HeroImage image={Image} />
+      <Container className='main'>
+        <Row>
+          <Col>
+            <h1>
+              Error
           </h1>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
