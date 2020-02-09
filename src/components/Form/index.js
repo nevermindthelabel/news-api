cf9e42ba66e84f9cfc3e313519a2baf34c731335
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-09 16:23:45 -0700

    destructured props

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const SearchForm = (props) => {
+const SearchForm = ({ handleChange, handleSubmit }) => {
 
   return (
-    <Form onSubmit={props.handleSubmit}>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Label for='Enter a search term'>Search for the latest news</Label>
-        <Input type='text' onChange={props.handleChange} placeholder='Search by news Topic' />
+        <Input type='text' onChange={handleChange} placeholder='Search by news Topic' />
       </FormGroup>
       <Button>Submit</Button>
     </Form>
