688e0604c4d692dc0d8f2c4dd206a460583f6600
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-08 21:52:34 -0700

    updated footer with image credit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,8 @@ import './style.css';
 const Footer = () => {
   return(
     <div className="footer">
-      <p>News powered by the <a href='https://newsapi.org/' rel='noopener noreferrer' target='_blank'>NewsAPI</a></p>
+      <p>News powered by the <a href='https://newsapi.org/' rel='noopener noreferrer' target='_blank'>NewsAPI</a> &nbsp;
+      Photo by <a href="https://unsplash.com/@romankraft">Roman Kraft</a> on <a href="https://unsplash.com">Unsplash</a></p>
     </div>
   )
 }
