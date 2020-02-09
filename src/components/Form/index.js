b674f67598eab43e030094e02f0128255281afcf
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-09 16:22:14 -0700

    cleaned up unused code

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,21 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-// import axios from 'axios';
-// import API from '../../util';
 
 const SearchForm = (props) => {
-  // const [searchTerm, setSearchTerm] = useState('');
-
-  // const handleSubmit = e => {
-  //   e.preventDefault();
-  //   if (!searchTerm) return;
-  //   setSearchTerm('');
-  //   axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${searchTerm}&apiKey=${API}`).then(data => console.log(data.data.articles))
-  // }
-
-  // const handleChange = e => {
-  //   setSearchTerm(e.target.value)
-  // }
 
   return (
     <Form onSubmit={props.handleSubmit}>
