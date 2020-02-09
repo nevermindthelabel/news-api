90d33ec503040c9a9a210514d94fdd7c7cfb20fa
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-08 21:13:44 -0700

    updated layout

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import image from '../../images/heroimage.jpg';
+import './style.css';
 
 const HeroImage = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <img src={image} alt="" />
+        <header>
+          <img src={image} alt="" />
+        </header>
       </div>
     </div>
   )
