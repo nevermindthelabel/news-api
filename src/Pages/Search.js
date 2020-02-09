cba0ae73ef4b2d6c771387bd8491613df9c99789
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-09 16:18:18 -0700

    added card compnent for search results

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -3,27 +3,44 @@ import { Container, Row, Col } from 'reactstrap';
 import Form from '../components/Form';
 import API from '../util';
 import axios from 'axios';
+import Spinner from '../components/Spinner';
+import Card from '../components/Card';
+import HeroImage from '../components/HeroImage';
+import Image from '../images/banner.png';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState();
+  const [articles, setArticles] = useState([]);
+  // const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${searchTerm}&apiKey=${API}`).then(data => {
-      console.log(`${data.data.articles}`)
-    })
-  }, [searchTerm])
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!searchTerm) return;
+    setSearchTerm('');
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${searchTerm}&apiKey=${API}`).then(data => setArticles(data.data.articles)); setSearchTerm('');
+  }
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value)
+  }
 
   return (
-    <Container className='main'>
-      <Row>
-        <Col>
-          <h1>Search</h1>
-        </Col>
-      </Row>
-      <Form searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-    </Container>
+    <>
+      <HeroImage image={Image} />
+      <Container className='main'>
+        <Row>
+          <Col>
+            <h1>Search</h1>
+          </Col>
+        </Row>
+        <Form handleSubmit={handleSubmit} handleChange={handleChange} />
+        <Row>
+          {articles.map((article, index) =>
+            <Card key={index} article={article} />)}
+        </Row>
+      </Container>
+    </>
   )
-
 }
 
 export default Search;
