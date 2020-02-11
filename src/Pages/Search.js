5a397956f425fb9d3ab57fab3db179720d98ee81
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-10 20:42:27 -0700

    removed unused import

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Form from '../components/Form';
 import API from '../util';
