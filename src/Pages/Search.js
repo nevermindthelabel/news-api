ccf92e792da863dfeacd79605fcd77abae1b6ae0
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-10 21:33:38 -0700

    added dropdown with some countries to search from

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Label } from 'reactstrap';
 import Form from '../components/Form';
 import API from '../util';
 import axios from 'axios';
@@ -9,17 +9,25 @@ import Image from '../images/banner.png';
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [dropdownValue, setDropdownValue] = useState();
   const [articles, setArticles] = useState([]);
+
+  const toggle = () => setDropdownOpen(prevState => !prevState)
 
   const handleSubmit = e => {
     e.preventDefault();
     if (!searchTerm) return;
     setSearchTerm('');
-    axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${searchTerm}&apiKey=${API}`).then(data => setArticles(data.data.articles));
+    axios.get(`https://newsapi.org/v2/top-headlines?country=${dropdownValue}&category=${searchTerm}&apiKey=${API}`).then(data => setArticles(data.data.articles));
   }
 
   const handleChange = e => {
     setSearchTerm(e.target.value)
+  }
+
+  const handleDropdownClick = e => {
+    setDropdownValue(e.target.value)
   }
 
   return (
@@ -31,7 +39,34 @@ const Search = () => {
             <h1>Search</h1>
           </Col>
         </Row>
-        <Form handleSubmit={handleSubmit} handleChange={handleChange} />
+        <Row>
+          <Col md="6">
+            <Form handleSubmit={handleSubmit} handleChange={handleChange} />
+          </Col>
+          <Col md="6">
+            <Label>Select Country</Label>
+            <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+              <DropdownToggle caret >
+                Country
+        </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem onClick={handleDropdownClick} value={'ae'}>United Arab Emirates</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'au'}>Australia</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'ca'}>Canada</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'cn'}>China</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'de'}>Germany</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'fr'}>France</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'gb'}>Great Britain</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'hk'}>Hong Kong</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'in'}>India</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'il'}>Israel</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'jp'}>Japan</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'mx'}>Mexico</DropdownItem>
+                <DropdownItem onClick={handleDropdownClick} value={'us'}>United States</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Col>
+        </Row>
         <Row>
           {articles.map((article, index) =>
             <Card key={index} article={article} />)}
