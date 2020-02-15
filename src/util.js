fb567c206723bcdc72dc1da7dcedf6bc443c9f55
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-14 22:42:10 -0700

    added country array

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
 const API = '7a915ef2180a43f2844db9159852e4ec'
 
-export default API;
+const countriesToSearchFrom = ['United Arab Emirates', 'Australia', 'Canada', 'China', 'China', 'Germany', 'France', 'Great Britain', 'Hong Kong', 'India', 'Israel', 'Japan', 'Mexico', 'United States'];
+
+export { API, countriesToSearchFrom };
